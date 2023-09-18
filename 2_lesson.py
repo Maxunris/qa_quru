909539efe18a1a0a809f87ecc6e6634678a14c1f
Max <Cheshir2451@gmail.com>
2023-09-18 22:26:11 +0300

    Тест для второго урока без размера окна

--- 2_lesson.py
+++ 2_lesson.py
@@ -10,4 +10,4 @@ def test_search_2(open_browser):
     browser.element('[class="ExCKkf z1asCe rzyADb"]').click()
     browser.element('[name="q"]').type('ыфа!2512афыа1йафывп!"№!"4').press_enter()
     browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
-    print("В поиске нет результатов")
+    print("В поиске нет результатов...")
