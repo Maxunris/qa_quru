5cd8e169f4b5b5196493e2ab368d0c1c6c84c847
max <Maxunris@gmail.com>
2023-09-28 20:19:39 +0300

    Правки

--- tests/test_search_browser.py
+++ tests/test_search_browser.py
@@ -4,10 +4,10 @@ from selene import have, browser
 def test_search_1(setting_browser):
     browser.open("https://www.google.com/")
     browser.element('[name="q"]').type('yashaka/selene').press_enter()
-    browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+    browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
 
 
-def test_search_2(setting_browser):
+def test_search_2():
     browser.element('[class="ExCKkf z1asCe rzyADb"]').click()
     browser.element('[name="q"]').type('ыфа!2512афыа1йафывп!"№!"4').press_enter()
     browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
